32c53ed7feb810be15a7fbd425a2e94cee1dcda3
pradeepjoseph7 <pradeepjoseph7@gmail.com>
2021-12-17 07:40:08 +0530

    Add files via upload

---
+++ streamlit_sharing_part1.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+
+st.write("My First Streamlit Web App")
+
+df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
+st.write(df)
+
+spectra = st.file_uploader("upload file", type={"csv", "txt"})
+if spectra is not None:
+    spectra_df = pd.read_csv(spectra)
+st.write(spectra_df)
