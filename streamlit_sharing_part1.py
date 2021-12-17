c21c7d38fec7848645e4034d2efd516790d3b30e
pradeepjoseph7 <pradeepjoseph7@gmail.com>
2021-12-17 07:48:03 +0530

    Update streamlit_sharing_part1.py

--- streamlit_sharing_part1.py
+++ streamlit_sharing_part1.py
@@ -8,6 +8,7 @@ df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
 spectra = st.file_uploader("upload file", type={"csv", "txt"})
-if spectra is not None:
-    spectra_df = pd.read_csv(spectra)
+# if spectra is not None:
+#     spectra_df = pd.read_csv(spectra)
+spectra_df = pd.read_csv(spectra)
 st.write(spectra_df)
