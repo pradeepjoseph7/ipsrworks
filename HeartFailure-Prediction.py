937d4ae611ce7e964b96709600c60e190f94f9a7
pradeepjoseph7 <pradeepjoseph7@gmail.com>
2022-01-11 21:02:35 +0530

    Update HeartFailure-Prediction.py

--- HeartFailure-Prediction.py
+++ HeartFailure-Prediction.py
@@ -11,17 +11,17 @@ testdata_df = pd.DataFrame()
 testdata = st.file_uploader("Please Upload the Unseen Test Data", type={"csv", "txt"})
 if testdata is not None:
   testdata_df = pd.read_csv(testdata)
-st.write("Number of records in the prediction data are : ")
-st.write(testdata_df.count())
-st.write(count_row = testdata_df.shape[0])  # Gives number of rows
-st.write(count_col = testdata_df.shape[1])
+  st.write("Number of records in the prediction data are : ")
+  st.write(testdata_df.count())
+  st.write(count_row = testdata_df.shape[0])  # Gives number of rows
+  st.write(count_col = testdata_df.shape[1])
 #modelload = load_model('Final_model')
 #unseen_predictions_new = predict_model(final, data=testdata_df)
 #st.altair_chart(testdata_df, use_container_width=False)
 #filename = 'Final_model.pkl'
-loaded_model = load_model('Final_model')
+  loaded_model = load_model('Final_model')
 #loaded_model = pickle.load(open(filename, 'rb'))
-predictions_df = predict_model(estimator=loaded_model, data=testdata_df)
+  predictions_df = predict_model(estimator=loaded_model, data=testdata_df)
 #predictions = predictions_df['Label'][0]
-st.write("Heart Failure Prediction Results are: ")
-st.write(predictions_df)
+  st.write("Heart Failure Prediction Results are: ")
+  st.write(predictions_df)
