e3f6f68f3a6f3c9f24a1d1add4d965ff7d56907c
pradeepjoseph7 <pradeepjoseph7@gmail.com>
2022-01-11 23:39:18 +0530

    Update HeartFailure-Prediction.py

--- HeartFailure-Prediction.py
+++ HeartFailure-Prediction.py
@@ -12,6 +12,7 @@ testdata = st.file_uploader("Please Upload the Unseen Test Data", type={"csv", "
 if testdata is not None:
   testdata_df = pd.read_csv(testdata)
   st.write("Number of records in the prediction data are : ")
+  st.dataframe(testdata_df)
   st.write(testdata_df.count())
   st.write(count_row = testdata_df.shape[0])  # Gives number of rows
   st.write(count_col = testdata_df.shape[1])
