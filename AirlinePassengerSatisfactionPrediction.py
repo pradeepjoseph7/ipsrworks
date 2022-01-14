a232681b25a7dcc198250b9a983b612da8be4c95
pradeepjoseph7 <pradeepjoseph7@gmail.com>
2022-01-14 11:38:29 +0530

    Update AirlinePassengerSatisfactionPrediction.py

--- AirlinePassengerSatisfactionPrediction.py
+++ AirlinePassengerSatisfactionPrediction.py
@@ -3,7 +3,7 @@ import pandas as pd
 #import pickle
 from pycaret.classification import load_model, predict_model
 
-st.write("Heart Failure Prediction Web App")
+st.write("Airline Passenger Satisfaction Prediction Web App")
 
 #df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 #st.write(df)
@@ -20,9 +20,9 @@ if testdata is not None:
 #unseen_predictions_new = predict_model(final, data=testdata_df)
 #st.altair_chart(testdata_df, use_container_width=False)
 #filename = 'Final_model.pkl'
-  loaded_model = load_model('Final_model')
+  loaded_model = load_model('Final_model_airline')
 #loaded_model = pickle.load(open(filename, 'rb'))
   predictions_df = predict_model(estimator=loaded_model, data=testdata_df)
 #predictions = predictions_df['Label'][0]
-  st.write("Heart Failure Prediction Results are: ")
+  st.write("Airline Passenger Satisfaction Prediction Results are: ")
   st.write(predictions_df)
